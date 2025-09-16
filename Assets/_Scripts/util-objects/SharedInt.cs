cb4e9dacb9bd7e25c2a31750366e3d6c0f435cd2
Katherine Russell <katherine.russell@arm.com>
2025-09-16 11:31:45 +0100

    feature: sharedint scriptable object

---
+++ Assets/_Scripts/util-objects/SharedInt.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SharedInt", menuName = "Util/SharedInt", order = 0)]
+public class SharedInt : ScriptableObject
+{
+    //Scriptable object containing a single number value, with a starting value when needed. Used to link the same data value 
+    //Between different game object so they are not dependent on eachother. e.g. timers, 
+
+    [SerializeField] int startingInt = 0;
+    int currentInt;
+
+    public int value { get => currentInt; set => currentInt = value; }
+
+    void Awake() { ResetValue(); }
+
+    void ResetValue() { currentInt = startingInt; } 
+
+}
