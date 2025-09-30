52dc25be89c6d1d4b77efd205a3a9dbf1da7f392
Katherine Russell <katherine.russell@arm.com>
2025-09-30 10:59:58 +0100

    feature: gold star object

---
+++ Assets/20-29 Scripts/22 Object Controllers/22.05 Gold Star/GoldStarController.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class GoldStarController : MonoBehaviour
+{
+
+    [SerializeField] LiveData liveData;
+
+    void Start()
+    {
+        GameObject star = GameObject.Find("GoldStar");
+        // Only one gold star is allowed in a level at a time.
+        if (star != gameObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        liveData.GoldStarCollected = true;
+        Destroy(gameObject);
+    }
+
+}
