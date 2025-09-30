77f4e979a359a401168bfb1a618b5ee10a2d978d
Katherine Russell <katherine.russell@arm.com>
2025-09-30 14:32:19 +0100

    feature: skydive spot object

---
+++ Assets/20-29 Scripts/22 Object Controllers/22.07 Skydive Spot/SkydiveSpotController.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class SkydiveSpotController : MonoBehaviour
+{
+    [SerializeField] LiveData liveData;
+    [SerializeField] int requiredHeight;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (liveData.distanceToGround >= requiredHeight)
+        {
+            liveData.skydiversDropped += 1;
+            Destroy(gameObject);
+        }
+        else
+        {
+            liveData.headerToShow = "Not high enough!";
+            liveData.messageToShow = $"You need to be {requiredHeight} above the ground!!";
+        }
+    }
+
+
+}
