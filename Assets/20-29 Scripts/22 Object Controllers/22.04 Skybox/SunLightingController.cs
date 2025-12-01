e17c7f8488f7c452c0142570f3ca4d4fd40d45aa
Katherine Russell <katherine.russell@arm.com>
2025-12-01 14:33:03 +0000

    feature: water shader

--- Assets/20-29 Scripts/22 Object Controllers/22.04 Skybox/SunLightingController.cs
+++ Assets/20-29 Scripts/22 Object Controllers/22.04 Skybox/SunLightingController.cs
@@ -8,13 +8,17 @@ public class SunLightingController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        float[] timesOfDay = { -12, -5, 20, 80};
+        float time = timesOfDay[Random.Range(0, timesOfDay.Length)];
+
         if (liveData.activeMission is null)
         {
-            transform.Rotate(new Vector3(60,0,0));
+            transform.Rotate(new Vector3(time, 0, 0));
         }
         else
         {
-            transform.Rotate(new Vector3((float)liveData.activeMission.timeOfDay,0,0));   
+            transform.Rotate(new Vector3((float)liveData.activeMission.timeOfDay, 0, 0));
         }
     }
 }
