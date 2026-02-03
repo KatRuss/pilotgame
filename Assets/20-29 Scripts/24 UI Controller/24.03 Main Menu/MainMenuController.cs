21f5e352c57ed0de72235a45d009a7452ce808e1
Katherine Russell <katherine.russell@arm.com>
2026-02-03 09:37:16 +0000

    fix: remove old hanger code

--- Assets/20-29 Scripts/24 UI Controller/24.03 Main Menu/MainMenuController.cs
+++ Assets/20-29 Scripts/24 UI Controller/24.03 Main Menu/MainMenuController.cs
@@ -28,43 +28,17 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] TextMeshProUGUI primaryObjectivesText;
     [SerializeField] TextMeshProUGUI secondaryObjectivesText;
 
-
-    [Header("Hanger Objects")]
-    [SerializeField] GameObject hangerMenu;
-    [SerializeField] TextMeshProUGUI planeBodyName;
-    [SerializeField] TextMeshProUGUI planeEngineName;
-    [SerializeField] TextMeshProUGUI planeFuelName;
-
-    List<PlaneBodyObject> planeBodies;
-    List<PlaneEngineObject> planeEngines;
-    List<PlaneFuelTankObject> planeFuelTanks;
-    int planeBodyIndex = 0;
-    int planeEngineIndex = 0;
-    int planeFuelTankIndex = 0;
-
     [Header("Mission List")]
     [SerializeField] MissionList missionList;
-
-    [Header("Progression List")]
-    [SerializeField] ProgressionList progressionList;
 
     private void Awake()
     {
         populateLevelList();
-        populateUnlockLists();
         mainMenu.SetActive(true);
-    }
-
-    void Start()
-    {
-        setUnlockLists();
     }
 
     void Update()
     {
-        planeBodyName.text = planeBodies[planeBodyIndex].partName;
-        planeEngineName.text = planeEngines[planeEngineIndex].partName;
-        planeFuelName.text = planeFuelTanks[planeFuelTankIndex].partName;
         PopulateLevelInformation();
     }
 
@@ -111,7 +85,6 @@ public class MainMenuController : MonoBehaviour
     public void ShowLevelMenu()
     {
         levelSelectMenu.SetActive(true);
-        hangerMenu.SetActive(false);
         mainMenu.SetActive(false);
         optionsMenu.SetActive(false);
     }
@@ -119,23 +92,13 @@ public class MainMenuController : MonoBehaviour
     public void ShowMainMenu()
     {
         levelSelectMenu.SetActive(false);
-        hangerMenu.SetActive(false);
         mainMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-    }
-
-    public void ShowHangerManu()
-    {
-        levelSelectMenu.SetActive(false);
-        hangerMenu.SetActive(true);
-        mainMenu.SetActive(false);
         optionsMenu.SetActive(false);
     }
 
     public void ShowOptionsMenu()
     {
         levelSelectMenu.SetActive(false);
-        hangerMenu.SetActive(false);
         mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
     }
@@ -143,128 +106,5 @@ public class MainMenuController : MonoBehaviour
     public void PlayLevel()
     {
         levelToLoad.value = levelToShow.value;
-    }
-
-    void populateUnlockLists()
-    {
-        planeBodies = getUnlockedBodies();
-        planeEngines = getUnlockedEngines();
-        planeFuelTanks = getUnlockedFuelTanks();
-    }
-
-    public void moveBodyIndex(int move)
-    {
-        if (planeBodyIndex + move > planeBodies.Count - 1)
-        {
-            planeBodyIndex = 0;
-        }
-        else if (planeBodyIndex + move < 0)
-        {
-            planeBodyIndex = Math.Max(0, planeBodies.Count - 1);
-        }
-        else
-        {
-            planeBodyIndex += move;
-        }
-        updatePlayerBody();
-    }
-
-    public void updatePlayerEngine()
-        { liveData.playerPlane.SetPlaneEngine(planeEngines[planeEngineIndex]); }
-
-    public void updatePlayerFuelTank()
-        {liveData.playerPlane.SetPlaneFuelTank(planeFuelTanks[planeEngineIndex]);}
-
-    public void updatePlayerBody()
-        {liveData.playerPlane.SetPlaneBody(planeBodies[planeBodyIndex]);}
-
-    public void moveEngineIndex(int move)
-    {
-        if (planeEngineIndex + move > planeEngines.Count - 1)
-        {
-            planeEngineIndex = 0;
-        }
-        else if (planeEngineIndex + move < 0)
-        {
-            planeEngineIndex = Math.Max(0, planeEngines.Count - 1);
-        }
-        else
-        {
-            planeEngineIndex += move;
-        }
-        updatePlayerEngine();
-    }
-
-    public void moveFuelTankIndex(int move)
-    {
-        if (planeFuelTankIndex + move > planeFuelTanks.Count - 1)
-        {
-            planeFuelTankIndex = 0;
-        }
-        else if (planeFuelTankIndex + move < 0)
-        {
-            planeBodyIndex = Math.Max(0, planeFuelTanks.Count - 1);
-        }
-        else
-        {
-            planeFuelTankIndex += move;
-        }
-        updatePlayerFuelTank();
-    }
-
-    void setUnlockLists()
-    {
-        PlaneEngineObject usedEngine = liveData.playerPlane.GetPlaneEngine();
-        PlaneBodyObject usedBody = liveData.playerPlane.GetPlaneBody();
-        PlaneFuelTankObject usedFuelTank = liveData.playerPlane.GetPlaneFuelTank();
-
-        planeEngineIndex = planeEngines.IndexOf(usedEngine);
-        planeBodyIndex = planeBodies.IndexOf(usedBody);
-        planeFuelTankIndex = planeFuelTanks.IndexOf(usedFuelTank);
-    }
-
-    List<PlaneEngineObject> getUnlockedEngines()
-    {
-        List<PlaneEngineObject> result = new();
-
-        foreach (var part in progressionList.items)
-        {
-            if (part.planePart is PlaneEngineObject && part.unlocked)
-            {
-                result.Add((PlaneEngineObject)part.planePart);
-            }
-        }
-
-        return result;
-    }
-
-    List<PlaneBodyObject> getUnlockedBodies()
-    {
-        List<PlaneBodyObject> result = new();
-
-        foreach (var part in progressionList.items)
-        {
-            if (part.planePart is PlaneBodyObject && part.unlocked)
-            {
-                result.Add((PlaneBodyObject)part.planePart);
-            }
-        }
-
-        return result;
-    }
-
-    List<PlaneFuelTankObject> getUnlockedFuelTanks()
-    {
-        List<PlaneFuelTankObject> result = new();
-
-        foreach (var part in progressionList.items)
-        {
-            if (part.planePart is PlaneFuelTankObject && part.unlocked)
-            {
-                result.Add((PlaneFuelTankObject)part.planePart);
-            }
-        }
-
-        return result;
     }
 }
